ace4ad1909722fc4b533f01f2065efd646a6445a
blunk21 <g.rockandroller@gmail.com>
2022-09-28 18:38:59 +0200

    Implement model (#1)
    * Initial commit with some model advancement
    
    * create model
    
    * created model object
    
    * .
    
    * Initialized all MVC elements
    
    * Implemented View
    
    * Created controller
    
    Co-authored-by: blunk21 <g.bereczki21@gmail.com>

---
+++ utg/main.py
@@ -0,0 +1,26 @@
+import tkinter
+from model import Model
+from view import View
+from controller import Controller
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("UART Thermostat")
+        model = Model()
+        view = View(self)
+        view.pack(fill="both")
+
+        controller = Controller()
+
+        view.set_controller(controller=controller)
+
+
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ utg/model.py
@@ -0,0 +1,61 @@
+import json
+from dataclasses import dataclass
+from os.path import exists
+
+MAX_NO_OF_ROOMS = 4
+CONFIG_FILE_NAME = "room_configs.json"
+
+
+@dataclass
+class Room():
+    nr: int
+    target_temp: float
+
+
+class Model():
+    def __init__(self) -> None:
+        self.rooms: dict = {"Rooms": []}
+        if self.check_for_existing_config():
+            self.load_config()
+        else:
+            self.initialize_config()
+
+    def check_for_existing_config(self) -> bool:
+        if exists(CONFIG_FILE_NAME):
+            return True
+        else:
+            return False
+
+    def initialize_config(self) -> None:
+        self.create_room_objects()
+        self.write_config_to_json()
+
+    def set_room_target_temp(self, room_no: int, target_temp: float) -> None:
+        pass
+
+    def load_config(self):
+        print("Loading configuration")
+        fo = open(CONFIG_FILE_NAME,"r")
+        self.rooms = json.load(fo)
+        fo.close()
+
+    def write_config_to_json(self):
+        """
+        with open(CONFIG_FILE_NAME, "w") as fo:
+            fo.write('')
+            for room in self.rooms:
+                json.dump(room, fo, indent=4)"""
+        fo = open(CONFIG_FILE_NAME, "w")
+        fo.truncate(0)
+        json.dump(self.rooms, fo, indent=4)
+        fo.close()
+
+    def create_room_objects(self) -> None:
+        print("Creating room objects")
+        for nr in range(MAX_NO_OF_ROOMS):
+            new_room: dict = {
+                "nr": nr+1, "target_temp": 22.5, "cooling":0}
+            self.rooms["Rooms"].append(new_room)
+
+    def get_target_temp(self, room_nr: int) -> float:
+        return float(self.rooms["Rooms"][room_nr-1]["target_temp"])
