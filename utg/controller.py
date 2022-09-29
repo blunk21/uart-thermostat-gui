3c54e923793b4c41d773024bb86b89ef86de1c56
blunk21 <g.bereczki21@gmail.com>
2022-09-29 18:11:15 +0200

    implemented controller

--- utg/controller.py
+++ utg/controller.py
@@ -1,4 +1,19 @@
 
 
+
 class Controller():
-    pass
+    def __init__(self, model, view):
+        self. model = model
+        self.view = view
+
+    def cooling_radio_change(self,number:int,val:int):
+        try:
+            self.model.rooms["Rooms"][number]["cooling"] = int(val)
+        except TypeError or ValueError:
+            self.view.raise_error("An error occured. Could not change cooling status.")
+
+    def save_configuration(self):
+        self.model.write_config_to_json()
+    
+    def set_target_temp(self,number:int,val:float):
+        self.model.set_target_temp(room_nr=number,val=float(val))
